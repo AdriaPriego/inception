3363caaa23e0d7e5fdd51c1b5c3698dd3384d664
apriego- <apriego-@42.com>
2024-03-04 11:38:20 +0000

    MariaDB func and Wordpress

--- srcs/requirements/nginx/tools/ssl_cert_generator.sh
+++ srcs/requirements/nginx/tools/ssl_cert_generator.sh
@@ -4,6 +4,6 @@ crt_file="/etc/ssl/certs/nginx-selfsigned.cert"
 if [ -e $key_file ] && [ -e $crt_file ]; then
 	echo "Certificate alredy exist";
 else
-	openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout $key_file -out $crt_file -subj "/C=ES/ST=Barcelona/L=Barcelona/O=42/CN=42";
+	openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout $key_file -out $crt_file -subj "/C=ES/ST=Barcelona/L=Barcelona/O=42/OU=Education/CN=apriego.42.fr";
 fi
 exec "$@"
